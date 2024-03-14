74e2926dba55c77c7b08803d3f46abe31c49d1a5
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-14 14:28:22 +0900

    Title: 20대의 평균나이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240314/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0, count =0;
+
+    while(1){
+        int a;
+        cin >> a;
+        if(a>=20 && a<30){
+            sum += a;
+            cout << sum;
+        }
+        else
+            break;
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << double(sum)/count;
+    return 0;
+}
