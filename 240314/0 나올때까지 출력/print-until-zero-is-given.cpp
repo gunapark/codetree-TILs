47233280ec574de4ca9f86709ec33bb78453e07b
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-14 13:07:55 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240314/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while(true){
+        int n;
+        cin >>n;
+        if(n==0)
+            break;
+        cout << n << endl;
+
+    }
+    return 0;
+}
