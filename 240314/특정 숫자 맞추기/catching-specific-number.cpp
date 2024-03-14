374eac4039e46aa2472a1e06836814cbc6b3be22
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-14 13:11:47 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240314/특정 숫자 맞추기/catching-specific-number.cpp
+++ 240314/특정 숫자 맞추기/catching-specific-number.cpp
@@ -6,14 +6,15 @@ int main() {
     while(1){
         int n;
         cin >> n;
-
+        if(n==25){
+            cout << "Good";
+            break;
+        }
         if(n<25)
             cout << "Higher" << endl;
         else if (n>25)
             cout << "Lower" << endl;
-        else
-            cout << "Good" << endl;
-            break;
+        
     }
     return 0;
 }
