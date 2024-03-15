21c0e75fcd25f8203151a6d4c247803b6c35601a
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-15 20:32:19 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/별 그리기/star-drawing.cpp
+++ 240315/별 그리기/star-drawing.cpp
@@ -18,7 +18,7 @@ int main() {
         for(int j=0;j<i;j++){
             cout << " ";
         }
-        for(int j=0;j<n-2*(i-1);j++){
+        for(int j=0;j<2*n-1-2*i;j++){
             cout << "*";
         }
         cout << endl;
