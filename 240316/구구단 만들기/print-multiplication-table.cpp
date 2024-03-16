688d65e4007c7ec34caaa6b213c8c623f39bbc49
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-16 19:09:10 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240316/구구단 만들기/print-multiplication-table.cpp
+++ 240316/구구단 만들기/print-multiplication-table.cpp
@@ -6,14 +6,15 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    for(int i=0;i<9;i++){
-        for(int j=0;j<b/2;j++){
-            cout << b-2*j <<" * "<< i+1 <<" = "<<(b-2*j)*(i+1);
-            if(j<b/2-1){
+    for(int i=1;i<=9;i++){
+        for(int j=b;j>=a;j-=2){
+            cout << j << " * " << i << " = " << i*j;
+            if(j!=a)
                 cout << " / ";
-            }
-        } 
+        }
         cout << endl;
-    }
+    } 
+    
+    
     return 0;
 }
