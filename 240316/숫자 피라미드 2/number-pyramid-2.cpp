be95ff5bd6b708926cbaca86ab4893624c0d9697
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-16 19:25:29 +0900

    Title: 숫자 피라미드 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240316/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt=0;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cnt++;
+            cout << cnt << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
