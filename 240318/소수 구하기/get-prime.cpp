cfe06e4da9d74b40342909902738addc2279932a
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-18 20:01:54 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/소수 구하기/get-prime.cpp
+++ 240318/소수 구하기/get-prime.cpp
@@ -9,7 +9,7 @@ int main() {
     for(int i=2;i<=n;i++){
         bool x=true;
         for(int j=2;j<i;j++){
-            if(i%j==0 && i>2)
+            if(i%j==0)
                 x = false;
         }
         if(x==true)
