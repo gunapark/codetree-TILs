7a392880aeaa2699d5ba3fdce6d4d04bbb75e085
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-18 19:59:23 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/소수 구하기/get-prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i=2;i<=n;i++){
+        bool x=true;
+        for(int j=3;j<i;j++){
+            if(i%j==0)
+                x = false;
+        }
+        if(x==true)
+            cout << i << " ";
+    }
+    return 0;
+}
