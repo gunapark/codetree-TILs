2008afe0b1f03e4b3feceefce65018399d7abb74
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-18 19:36:20 +0900

    Title: 3n + 1 수열 m번, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m;
+    cin >> m;
+
+    for(int i=0;i<m;i++){
+        int n, cnt=0;
+        cin >> n;
+        while(n!=1){
+            if(n%2==0){
+                n /=2;
+            } else
+                n = n*3+1;
+            cnt++;
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
