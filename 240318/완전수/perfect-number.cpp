669d3e613020c29b96e662e73def052257c9bfe6
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-18 19:44:44 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/완전수/perfect-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int start, end, count_num=0;
+    cin >> start >> end;
+
+    for(int i=start;i<=end;i++){
+        int cnt=0;
+        for(int j=1;j<i;j++){
+            if(i%j==0){
+                cnt +=j;
+            }
+        }
+        if(cnt==i){
+            count_num ++;
+        }
+    }
+    cout << count_num;
+    return 0;
+}
