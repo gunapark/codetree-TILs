aef990d5d50a99c512c34401f05e9c23d0ff83da
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-21 21:51:56 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/숫자들의 배수/multiple-of-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int cnt=0;
+    cin >> arr[0];
+
+    for(int i=1;i<10;i++){
+        arr[i]= arr[0]*(i+1);
+    }
+    for(int i=0;i<10;i++){
+        cout << arr[i] << " ";
+        if(arr[i]%5==0){
+            cnt++;
+        }
+        if(cnt==2){
+            break;
+        }
+    }
+
+    
+    return 0;
+}
