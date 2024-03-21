0de0c9f114780cf3e9bb2461bdce1f1bbdbb7a83
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-21 21:34:20 +0900

    Title: 제곱하여 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        arr[i] = arr[i]*arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
