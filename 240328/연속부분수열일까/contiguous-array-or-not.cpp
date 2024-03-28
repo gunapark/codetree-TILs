d9717307cf2f9ef2450c8a6318626589171f8d70
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-28 23:15:05 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //A, B 수열의 원소 개수 입력
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    int arrA[n1];
+    int arrB[n2];
+
+    for(int i=0;i<n1;i++){
+        cin >> arrA[i];
+    }
+    for(int i=0;i<n2;i++){
+        cin >> arrB[i];
+    }
+    int j;
+    for(int i=0;i<n1;i++){
+        if(arrA[i]==arrB[0]){
+            for(j=1;j<n2;j++){
+                if(arrA[i+j]==arrB[j]){
+                    continue;
+                } else{
+                    break;
+                }
+            } if(j==n2){
+                cout << "Yes";
+                break;
+            } 
+        }
+    }
+    cout << "No";
+    return 0;
+}
