bb8a0097a13ae736a3882ab5f52efb23dbc41209
gunapark <148926641+gunapark@users.noreply.github.com>
2024-03-28 23:41:01 +0900

    Title: 999 또는 -999, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/999 또는 -999/999-or-999.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[100];
+    int i;
+    for(i=0;i<100;i++){
+        int a;
+        cin >> a;
+        if(a==999 || a== -999){
+            break;
+        }
+        arr[i]=a;
+    }
+    
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int j=1;j<i;j++){
+        if(arr[j]>max){
+            max = arr[j];
+        } else {
+            min = arr[j];
+        }
+    }
+    cout << max << " " << min;
+    return 0;
+}
