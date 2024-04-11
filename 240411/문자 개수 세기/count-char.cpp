2ee5d9f7fd25a0790a06e0ae7d31d0192fd34103
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-11 22:24:46 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/문자 개수 세기/count-char.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    getline(cin,str);
+    char a;
+    cin >> a;
+    int cnt=0;
+
+    for(int i=0;i<str.length();i++){
+        if(str[i]==a)
+            cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
