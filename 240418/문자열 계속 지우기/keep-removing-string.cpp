d25fc848dbbf3a611121f88e70722764449d7535
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-18 19:30:14 +0900

    Title: 문자열 계속 지우기, Time: 1076ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240418/문자열 계속 지우기/keep-removing-string.cpp
+++ 240418/문자열 계속 지우기/keep-removing-string.cpp
@@ -14,7 +14,7 @@ int main() {
     int len_B = B.length();
     int i;
 
-    for(i=0;i<=len_A;i++){
+    for(i=0;i<=A.length();i++){
         bool a = false;
         for(int j=0;j<len_B;j++){
             if(A[i+j]==B[j]){
@@ -24,7 +24,6 @@ int main() {
         }
         if(a==true){
             A.erase(i,len_B);
-            len_A -= len_B;
             i=0;
         }
     }
