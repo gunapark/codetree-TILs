c7922531eb16f1c38512572f416bc49d7ffb5cf1
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-18 19:19:29 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240418/문자열 계속 지우기/keep-removing-string.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A;
+    string B;
+    
+    cin >> A >> B;
+
+    
+    int len_A = A.length();
+    int len_B = B.length();
+
+    for(int i=0;i<=len_A-len_B;i++){
+        bool a = false;
+        for(int j=0;j<len_B;j++){
+            if(A[i+j]==B[j]){
+                a = true;
+            } else
+                a = false;
+        }
+        if(a==true){
+            A.erase(i,len_B);
+            len_A -= len_B;
+            i=0;
+        }
+        
+    }
+
+    cout << A;
+
+    return 0;
+}
