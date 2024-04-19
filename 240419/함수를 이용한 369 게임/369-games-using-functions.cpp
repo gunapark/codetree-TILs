c546d1085e15436dc06b30bb7d2447b0e2149781
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-19 23:49:15 +0900

    Title: 함수를 이용한 369 게임, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240419/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240419/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool bool_369(int n){
-    return n%10 == 3 || n%10 == 6 || n%10 == 9 || n/10 == 3 || n/10 == 6 || n/10 == 9;
+
+bool dffd(int n){
+    for(int i=1;i<=n;i*=10){
+        if((n/i)%10==3 || (n/i)%10==6 || (n/i)%10==9){
+            return true;
+        }
+    }
+    return false;
 }
 
-bool num_check(int n){
-    return n%3 == 0 || bool_369(n);
-}
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -15,10 +18,12 @@ int main() {
     cin >> a >> b;
 
     for(int i=a;i<=b;i++){
-        if(num_check(i)) cnt ++;
+        if(dffd(i) || i%3==0){
+            cnt ++;
+        }
     }
 
     cout << cnt;
-
+    
     return 0;
 }
