187aaad199b532ff0d61202e80ddc968958008c4
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-30 19:36:42 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool is_palindrome(string &A){
+    int str_len = A.length();
+    bool same = false;
+    for(int i=0;i<str_len/2;i++){
+        if(A[0]==A[str_len-1-i]) same = true;
+        else false;
+    }
+    return same;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A;
+    cin >> A;
+
+    if(is_palindrome(A)) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
