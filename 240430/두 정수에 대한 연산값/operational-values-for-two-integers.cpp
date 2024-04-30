696efebc611077d618a53b070d7c09984da49fdc
gunapark <148926641+gunapark@users.noreply.github.com>
2024-04-30 19:45:39 +0900

    Title: 두 정수에 대한 연산값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240430/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void modify(int &a, int &b){
+    if(a>b){
+        a +=25;
+        b *=2;
+    } else{
+        b +=25;
+        a *=2;
+    }
+}
+
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    modify(a, b);
+    
+    cout << a <<" " << b;
+    return 0;
+}
