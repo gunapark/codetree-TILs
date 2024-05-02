65ef6664297d9d38d9da7b32131e396ba7e27a7f
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-03 01:37:10 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240503/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int maximum(int n,int *arr){
+    if(n==0) return arr[0];
+    return max(arr[n],maximum(n-1,arr));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    cout << maximum(n-1,arr);
+    return 0;
+}
