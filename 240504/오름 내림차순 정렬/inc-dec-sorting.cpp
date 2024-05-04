64b75f938c85683732d40f2eca5ce2bdd14c3c40
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-04 20:35:55 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a, int b){
+    return a>b;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    sort(arr, arr+n, compare);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
