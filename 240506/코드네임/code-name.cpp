3eb85f21bfbaf742dbe438cb06ebc8e69944010f
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-06 20:42:50 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240506/코드네임/code-name.cpp
+++ 240506/코드네임/code-name.cpp
@@ -20,7 +20,7 @@ int main() {
         cin >> fbi[i].codename >> fbi[i].score;
     }
 
-    int min = 0, cnt = 0;
+    int min = 100, cnt = 0;
     for(int i=0;i<5;i++){
         if(fbi[i].score<min){
             min = fbi[i].score;
