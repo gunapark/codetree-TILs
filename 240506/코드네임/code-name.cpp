d937d2436267ff9a127617c87cf178e045d51c19
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-06 20:41:54 +0900

    Title: 코드네임, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240506/코드네임/code-name.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class agent{
+    public:
+        string codename;
+        int score;
+
+    agent(string _codename="", int _score=0){
+        this->codename = _codename;
+        this->score = _score;
+    }
+};
+
+int main() {
+    agent *fbi = new agent[5];
+
+    for(int i=0;i<5;i++){
+        cin >> fbi[i].codename >> fbi[i].score;
+    }
+
+    int min = 0, cnt = 0;
+    for(int i=0;i<5;i++){
+        if(fbi[i].score<min){
+            min = fbi[i].score;
+            cnt = i;
+        }
+    }
+    cout << fbi[cnt].codename << " " << fbi[cnt].score;
+    return 0;
+}
