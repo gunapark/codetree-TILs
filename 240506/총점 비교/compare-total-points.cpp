5b8e2d93ea70a087a5e0fcb703c07e05be6816ba
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-06 22:40:32 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240506/총점 비교/compare-total-points.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class student{
+    public:
+        string name;
+        int kor, eng, math;
+        int sum;
+        student(string name, int kor, int eng, int math){
+            this->name=name;
+            this->kor = kor;
+            this->eng = eng;
+            this->math = math;
+            this->sum = kor+eng+math;
+        }
+        student(){};
+};
+
+bool cmp(student a, student b){
+    return a.sum<b.sum;
+}
+int main() {
+    int n;
+    cin >> n;
+    student students[n];
+    
+    for(int i=0;i<n;i++){
+        cin >> students[i].name >> students[i].kor >> students[i].eng >> students[i].math;
+    }
+
+    sort(students,students+n,cmp);
+
+    for(int i=0;i<n;i++){
+        cout << students[i].name << " "<< students[i].kor << " "<< students[i].eng <<" "<< students[i].math<<endl;
+    }
+    return 0;
+}
