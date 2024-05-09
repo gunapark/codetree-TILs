f2acb0e0c44686ac9bf163ccd7d73aed7b83a0af
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-10 00:38:19 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240510/그 요일은/the-day-of-the-day.cpp
+++ 240510/그 요일은/the-day-of-the-day.cpp
@@ -25,18 +25,15 @@ int main() {
             int_dydlf = i;
         }
     }
-
+    cout << int_dydlf << endl;
 
     int diff = total_day(m2,d2) - total_day(m1,d1);
+    cout << diff << endl;
+    int day_of_end = (diff%7+1)%7;
     int week=0;
 
     week = diff/7;
-
-
-    diff %= 7;
-
-
-    if(diff+1>=1){
+    if(day_of_end<=int_dydlf){
         week++;
     }
 
