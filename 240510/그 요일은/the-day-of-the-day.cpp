b0d163b4c78ac9034d57a37a70533b90dac8a990
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-10 00:53:19 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240510/그 요일은/the-day-of-the-day.cpp
+++ 240510/그 요일은/the-day-of-the-day.cpp
@@ -27,11 +27,11 @@ int main() {
     }
 
     int diff = total_day(m2,d2) - total_day(m1,d1);
-    int day_of_end = (diff%7+1)%7;
+    int day_of_week_of_end = (diff+1)%7%7;
     int week=0;
 
     week = diff/7;
-    if(day_of_end<=int_dydlf){
+    if(day_of_end>=int_dydlf){
         week++;
     }
 
