4ec3a51cdcffb49cd3d1ca152a04849b7d0439a2
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-17 17:58:23 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240517/벌금은 누구에게/who-will-pay.cpp
+++ 240517/벌금은 누구에게/who-will-pay.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class Student{
-    public:
-        int num;
-        int penalty;
-        Student(int num=0,int penalty=0){
-            this->num = num;
-            this->penalty=penalty;
-        }
-};
-
 int main() {
     int n,m,k;
     cin >> n >> m >> k;
-    Student *students=new Student[n+1];
-    for(int i=1;i<=n;i++){
-        students[i].num = i;
-    }
+    int students[n+1]={};
+    
     for(int i=0;i<m;i++){
-        int pen;
-        cin>>pen;
-        students[pen].penalty++;
+        int penalty;
+        cin >> penalty;
+        students[penalty]++;
+        if(students[penalty]>=k){
+            cout << penalty;
+            return 0;
+        } 
+        
     }
 
-    for(int i=1;i<=n;i++){
-        if(students[i].penalty>=k){
-            cout << students[i].num;
-            return 0;
-        }
-    }
     cout << -1;
     return 0;
 }
