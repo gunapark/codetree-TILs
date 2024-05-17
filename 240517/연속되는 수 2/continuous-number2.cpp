12c12620f8481ca0f90a1be46f6dc6a977b277af
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-17 10:36:35 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240517/연속되는 수 2/continuous-number2.cpp
+++ 240517/연속되는 수 2/continuous-number2.cpp
@@ -9,15 +9,14 @@ int main() {
     for(int i=0;i<n;i++){
         cin >> arr[i];
     }
-    sort(arr, arr+n);
+
 
     int max_cnt=0, cnt=0;
-    for(int i=0;i<n;i++){
-        if(i==0 || arr[i]==arr[i-1]){
+    for(int i=0;i<n-1;i++){
+        if(i==0||arr[i]==arr[i-1]){
             cnt++;
             if(cnt>max_cnt) max_cnt=cnt;
-        } else cnt=0;
-
+        } else cnt=1;
     }
     cout << max_cnt;
     return 0;
