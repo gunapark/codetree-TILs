cf1eebfb9304213d1d37c4df7e0a5fefe4635b76
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-17 10:45:17 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240517/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int cnt=0,max_cnt=0;
+    for(int i=0;i<n;i++){
+        if(i==0 || (arr[i]>0 && arr[i-1]>0) || (arr[i]<0 && arr[i-1]<0)){
+            cnt++;
+        } else cnt =1;
+        max_cnt = max(cnt, max_cnt);
+    }
+    cout << max_cnt;
+    return 0;
+}
