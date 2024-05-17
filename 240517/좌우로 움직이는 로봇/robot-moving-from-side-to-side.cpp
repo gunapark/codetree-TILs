4cc3c48e60636e48d47d0ec62f22ba1c040ce618
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-17 20:17:34 +0900

    Title: 좌우로 움직이는 로봇, Time: 48ms, Memory: 7MB, Status: Wrong Answer - CodeTree

--- 240517/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240517/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -14,14 +14,16 @@ int main() {
         char d;
         cin >> t >> d;
         for(int i=0;i<t;i++){
-            if(d=='R') A[time_a+t]=A[time_a+t-1]+1;
-            else A[time_a+t]=A[time_a+t-1]-1;
+            if(d=='R') A[time_a+i]=A[time_a+i-1]+1;
+            else A[time_a+i]=A[time_a+i-1]-1;
         }
         time_a +=t;
     }
+
     for(int i=time_a;i<1000000;i++){
         A[i]=A[time_a-1];
     }
+
     
     int time_b=1;
     for(int i=0;i<m;i++){
@@ -29,8 +31,8 @@ int main() {
         char d;
         cin >> t >> d;
         for(int i=0;i<t;i++){
-            if(d=='R') B[time_b+t]=B[time_b+t-1]+1;
-            else B[time_b+t]=B[time_b+t-1]-1;
+            if(d=='R') B[time_b+i]=B[time_b+i-1]+1;
+            else B[time_b+i]=B[time_b+i-1]-1;
         }
         time_b +=t;
     }
@@ -38,6 +40,8 @@ int main() {
     for(int i=time_b;i<1000000;i++){
         B[i]=B[time_b-1];
     }
+
+
     int cnt=0;
     for(int i=1;i<max(time_a,time_b);i++){
         if(A[i]==B[i] && A[i-1]!=B[i-1])cnt++;
