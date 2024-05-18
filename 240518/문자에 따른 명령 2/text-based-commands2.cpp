dd1054d1fa26f265098c8e097f22580aa6e2db5d
gunapark <148926641+gunapark@users.noreply.github.com>
2024-05-18 20:33:59 +0900

    Title: 문자에 따른 명령 2, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240518/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240518/문자에 따른 명령 2/text-based-commands2.cpp
@@ -5,23 +5,21 @@ using namespace std;
 int main() {
     int x=0, y=0;
     int dx[4]={0,1,0,-1}, dy[4]={1,0,-1,0};
-    char dir;
-    string F;
+    string order;
     int head=0;
-    cin >> dir >> F;
-    int n=F.length();
+    cin >> order; 
+    int n=order.length();
 
-    if(dir=='R'){
-        for(int i=0;i<n;i++){
-            head=(head+1)%4;
-            x+=dx[head]; y+=dy[head];
-        }
-    }else{
-        for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){
+        if(order[i]=='L'){
             head=(head-1+4)%4;
+        }else if(order[i]=='R'){
+            head=(head+1)%4;
+        }else{
             x+=dx[head]; y+=dy[head];
         }
     }
+
     cout << x <<" "<< y;
     
     return 0;
