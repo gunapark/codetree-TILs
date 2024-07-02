bd7accd644202151fe83a254578feee0cce036c2
gunapark <148926641+gunapark@users.noreply.github.com>
2024-07-03 07:02:45 +0900

    Title: 정수 찾기, Time: 206ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240703/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int n,m;
+unordered_set<int> arr1;
+
+int main() {
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int num;
+        cin >> num;
+        arr1.insert(num);
+    }
+    cin >> m;
+    for(int i=0;i<m;i++){
+        int num;
+        cin >> num;
+        if(arr1.find(num)!=arr1.end()){
+            cout << 1 << endl;
+        }else cout << 0 << endl;
+    }
+    return 0;
+}
