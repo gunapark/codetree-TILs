33788118ee80c945786293aedc212edfa022180d
바꼬나 <148926641+gunapark@users.noreply.github.com>
2024-07-04 23:59:56 +0900

    Title: 트리 노드 제거, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240704/트리 노드 제거/remove-tree-node.cpp
+++ 240704/트리 노드 제거/remove-tree-node.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define MAX_N 50
 int n;
+int root_node;
 
 vector<int> edges[MAX_N];
 bool visit[MAX_N];
@@ -24,26 +25,21 @@ int count_leaf_node(int node){
 int main() {
     int n;
     cin >> n;
-
     for(int i=0;i<n;i++){
         int a;
         //2번째 줄을 입력을 받음.
         cin >> a;
         //루트 노드는 아무것도 하지 않음.
-        if(a==-1) continue;
-
-        //루트 노드가 아니면 a노드의 vector에 i노드가 자손임을 추가해줌.
-        edges[a].push_back(i);
+        if(a==-1) root_node=i;
+        else{
+            edges[11].push_back(i);
+        }
         //쌍방으로 해줄 필요는 없다고 생각함.
     }
     //삭제할 노드를 입력 받음.
     int dnode;
     cin >> dnode;
 
-    if(dnode==0){
-        cout << 0;
-        return 0;
-    }
     //node들을 찾아다니면서 dnode가 있는지 탐색
     for(int i=0;i<n;i++){
         for(int j=0;j<(int)edges[i].size();j++){
@@ -56,9 +52,9 @@ int main() {
     for(int i=0;i<n;i++){
         visit[i]=false;
     }
-    visit[0]=true;
+    visit[root_node]=true;
 
-    cout << count_leaf_node(0);
+    cout << count_leaf_node(root_node);
 
     return 0;
 }
