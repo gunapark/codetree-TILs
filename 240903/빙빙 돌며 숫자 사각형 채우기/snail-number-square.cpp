b525751de322eece2ffbfbae62dd64fe27c2ff95
바꼬나 <148926641+gunapark@users.noreply.github.com>
2024-09-03 23:53:14 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -15,7 +15,7 @@ int main() {
     for(int i=1;i<=n*m;i++){
         int nt_x = x+dx[dir];
         int nt_y = y+dy[dir];
-        if(nt_x>m || nt_y>n || check[nt_x][nt_y]|| nt_x<1 || nt_y<1){
+        if(nt_x>n || nt_y>m || check[nt_x][nt_y]|| nt_x<1 || nt_y<1){
             dir = (dir+1)%4;
         }
         x += dx[dir];
@@ -24,11 +24,13 @@ int main() {
         check[x][y] = true;
     }
 
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            cout << arr[j][i] << ' ';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout << arr[i][j] << ' ';
         }
         cout << endl;
     }
+
+
     return 0;
 }
